c38af01208df9cd31c4ff20521097d75d97ee548
yiwen.wu <yiwen.wu@shopee.com>
2019-10-18 18:08:35 +0800

    latest version

--- replenishment_ATP_Summary.py
+++ replenishment_ATP_Summary.py
@@ -3,6 +3,7 @@ from __future__ import division
 import argparse
 import datetime as dt
 import os
+import json
 # from noattach import NoAttach
 # import sendmail
 # from pyspark.sql.types import *
@@ -15,7 +16,9 @@ import warnings
 import pandas as pd
 # from unidecode import unidecode
 import sql_queries
-from helper_functions_correct import append_df_to_excel, write_to_csv, Regional_ATP_rewrite, Regional_Replenishment_rewrite
+from helper_functions_correct import append_df_to_excel, write_to_csv
+from helper_functions_correct import Regional_ATP_rewrite, Regional_Replenishment_rewrite, save_report_path, \
+    load_report_path
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 from process_ATP_correct_v3 import process_atp_df, process_replenishment_df, process_raw_data_frames
 
@@ -32,10 +35,9 @@ from process_ATP_correct_v3 import process_atp_df, process_replenishment_df, pro
 warnings.simplefilter("ignore")
 
 
-def main(today_date, country_list, data_queried, files_processed, replenishment_done, reports_written, summary_completed):
+def main(today_date, country_list, data_queried, files_processed, replenishment_done, reports_written,
+         summary_completed):
     # Processing for each country
-    ATP_reports_path = {'MY':'', 'TH':'','TW':'','ID':'','VN':'','PH':''}
-    Replenishment_reports_path = {'MY':'', 'TH':'','TW':'','ID':'','VN':'','PH':''}
     if replenishment_done:
         files_processed = True
         data_queried = True
@@ -97,17 +99,23 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
 
         print("Querying Database for items....")
         if not data_queried:
-            write_to_csv(sql_queries.get_cogs(country, TODAY), file_names['cogs'])
+            cogs_color = sql_queries.get_cogs_color(country, TODAY)
+            cogs = cogs_color.select('sku_id', 'grass_date', 'cogs')
+            color = cogs_color.select('sku_id', 'color', 'stock_on_hand', 'grass_date')
+            write_to_csv(cogs, file_names['cogs'])
             write_to_csv(sql_queries.get_purchase_type(country), file_names['purchase_type'])
-            write_to_csv(sql_queries.get_brands(country, str_four_weeks_ago_date, str_two_weeks_ago_date, str_yesterday_date),file_names['brand'])
-            write_to_csv(sql_queries.get_color(country, TODAY), file_names['new_color'])
+            write_to_csv(
+                sql_queries.get_brands(country, str_four_weeks_ago_date, str_two_weeks_ago_date, str_yesterday_date),
+                file_names['brand'])
+            write_to_csv(color, file_names['new_color'])
             write_to_csv(sql_queries.get_pr_po_inbound(country), file_names['pr_po_inbound'])
             write_to_csv(sql_queries.get_cat_map(country), file_names['cat_cluster'])
             write_to_csv(sql_queries.get_cfs(country, TODAY, str_four_weeks_ago_date), file_names['cfs_wk4'])
             write_to_csv(sql_queries.get_cfs(country, TODAY, str_two_weeks_ago_date), file_names['cfs_wk2'])
             write_to_csv(sql_queries.get_cfs(country, TODAY, str_yesterday_date), file_names['cfs_ytd'])
             write_to_csv(sql_queries.get_item_profile(country), file_names['item_profile_history'])
-            write_to_csv(sql_queries.get_replenishment_ext(country, str_four_weeks_ago_date, str_two_weeks_ago_date,str_yesterday_date), file_names['ext'])
+            write_to_csv(sql_queries.get_replenishment_ext(country, str_four_weeks_ago_date, str_two_weeks_ago_date,
+                                                           str_yesterday_date), file_names['ext'])
             write_to_csv(sql_queries.get_replenishment_core(country, TODAY), file_names['replenishment'])
 
             print("Data Queried and converted to csv files in 'input_file' directory!")
@@ -116,24 +124,42 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
         if not replenishment_done:
             print("Reading input files after conversion...")
 
-            brand = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'brand.csv'), encoding='utf-8-sig', index_col=False)
-            cogs = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'cogs.csv'), encoding='utf-8-sig', index_col=False)
-            new_color = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'new_color.csv'), encoding='utf-8-sig', index_col=False)
-            replenishment = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'replenishment.csv'), encoding='utf-8-sig', index_col=False)
-            ext = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'ext.csv'), encoding='utf-8-sig', index_col=False)
-            pr_po_inbound = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'pr_po_inbound.csv'), encoding='utf-8-sig', index_col=False)
-            item_profile_history = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'item_profile_history.csv'), encoding='utf-8-sig', index_col=False)
-            cat_cluster = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'cat_cluster.csv'), encoding='utf-8-sig', index_col=False)
-            cfs_wk2 = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'cfs_wk2.csv'), encoding='utf-8-sig', index_col=False)
-            cfs_wk4 = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'cfs_wk4.csv'), encoding='utf-8-sig', index_col=False)
-            cfs_ytd = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'cfs_ytd.csv'), encoding='utf-8-sig', index_col=False)
-            purchase_type = pd.read_csv('{}'.format(input_file_path+'/'+country+'_'+'purchase_type.csv'), encoding='utf-8-sig', index_col=False)
+            brand = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'brand.csv'), encoding='utf-8-sig',
+                                index_col=False)
+            print('brand!')
+            cogs = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'cogs.csv'), encoding='utf-8-sig',
+                               index_col=False)
+            print('cogs!')
+            new_color = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'new_color.csv'),
+                                    encoding='utf-8-sig', index_col=False)
+            print('new_color!')
+            replenishment = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'replenishment.csv'),
+                                        encoding='utf-8-sig', index_col=False)
+            print('replenishment!')
+            ext = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'ext.csv'), encoding='utf-8-sig',
+                              index_col=False)
+            pr_po_inbound = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'pr_po_inbound.csv'),
+                                        encoding='utf-8-sig', index_col=False)
+            item_profile_history = pd.read_csv(
+                '{}'.format(input_file_path + '/' + country + '_' + 'item_profile_history.csv'), encoding='utf-8-sig',
+                index_col=False)
+            cat_cluster = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'cat_cluster.csv'),
+                                      encoding='utf-8-sig', index_col=False)
+            cfs_wk2 = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'cfs_wk2.csv'),
+                                  encoding='utf-8-sig', index_col=False)
+            cfs_wk4 = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'cfs_wk4.csv'),
+                                  encoding='utf-8-sig', index_col=False)
+            cfs_ytd = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'cfs_ytd.csv'),
+                                  encoding='utf-8-sig', index_col=False)
+            purchase_type = pd.read_csv('{}'.format(input_file_path + '/' + country + '_' + 'purchase_type.csv'),
+                                        encoding='utf-8-sig', index_col=False)
 
             print("All files read!")
 
             ext = ext.sort_values('sourcing_status').drop_duplicates(['sku_id', 'grass_date'], keep='first')
 
-            purchase_type, ext, replenishment, pr_po_inbound, new_color = process_raw_data_frames(purchase_type, ext, brand,
+            purchase_type, ext, replenishment, pr_po_inbound, new_color = process_raw_data_frames(purchase_type, ext,
+                                                                                                  brand,
                                                                                                   replenishment,
                                                                                                   pr_po_inbound,
                                                                                                   new_color)
@@ -193,14 +219,13 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
                                                                                              wk_4_atp_df
                                                                                              )
 
-
-
         if not reports_written:
             if replenishment_done:
                 new_folder = './' + country + '/input_files/'
-                replenishment_prod_info = pd.read_csv(new_folder + 'replenishment_tab_v4.csv', encoding='utf-8-sig')
-                inbounded_tab = pd.read_csv(new_folder + "inbounded_tab.csv", encoding='utf-8-sig')
-                rop_to_pr_tab = pd.read_csv(new_folder + "rop_to_pr.csv", encoding='utf-8-sig')
+                replenishment_prod_info = pd.read_csv(new_folder + 'replenishment_tab_v4.csv', encoding='utf-8-sig',
+                                                      index_col=0)
+                inbounded_tab = pd.read_csv(new_folder + "inbounded_tab.csv", encoding='utf-8-sig', index_col=0)
+                rop_to_pr_tab = pd.read_csv(new_folder + "rop_to_pr.csv", encoding='utf-8-sig', index_col=0)
                 ytd_fn = "{}/ytd_product_info.csv".format(country)
                 ytd_atp_df = pd.read_csv(ytd_fn, na_values="n.a.", encoding='utf-8-sig')
                 ytd_write = process_dfs_before_write(ytd_atp_df)
@@ -209,15 +234,15 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
                 wk_2_write = process_dfs_before_write(wk_2_atp_df)
 
             r_write = replenishment_prod_info.applymap(
-                lambda x: re.sub(ILLEGAL_CHARACTERS_RE, '', x) if isinstance(x, str) else x)
+                lambda x: re.sub(ILLEGAL_CHARACTERS_RE, '', x) if isinstance(x, str) or isinstance(x, unicode) else x)
             r_write = r_write.applymap(lambda x: x.encode('unicode_escape').
-                                           decode('utf-8-sig') if isinstance(x, str) else x)
-            wk_2_write = wk_2_write.applymap(
-                lambda x: re.sub(ILLEGAL_CHARACTERS_RE, '', x) if isinstance(x, str) else x)
-            wk_2_write = wk_2_write.applymap(lambda x: x.encode('unicode_escape').
                                        decode('utf-8-sig') if isinstance(x, str) else x)
+            wk_2_write = wk_2_write.applymap(
+                lambda x: re.sub(ILLEGAL_CHARACTERS_RE, '', x) if isinstance(x, str) or isinstance(x, unicode) else x)
+            wk_2_write = wk_2_write.applymap(lambda x: x.encode('unicode_escape').
+                                             decode('utf-8-sig') if isinstance(x, str) else x)
             ytd_write = ytd_write.applymap(
-                lambda x: re.sub(ILLEGAL_CHARACTERS_RE, '', x) if isinstance(x, str) else x)
+                lambda x: re.sub(ILLEGAL_CHARACTERS_RE, '', x) if isinstance(x, str) or isinstance(x, unicode) else x)
             ytd_write = ytd_write.applymap(lambda x: x.encode('unicode_escape').
                                            decode('utf-8-sig') if isinstance(x, str) else x)
             r_write_dict = {
@@ -236,10 +261,17 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
             r_output_file = "{}{}_{}_{}.xlsx".format(country_folder_path, country, file_date,
                                                      r_file_name[r_file_name.rfind('/') + 1:
                                                                  r_file_name.rfind("Template") - 1])
+            # save the reports path into json file
+            Replenishment_reports_path = load_report_path('Replenishment_reports_path')
             Replenishment_reports_path[country] = r_output_file
+            save_report_path('Replenishment_reports_path', Replenishment_reports_path)
+
             filename = "./templates/ATP_Template.xlsx"
             output_file = "{}{}_{}_ATP Report.xlsx".format(country_folder_path, country, file_date)
+            ATP_reports_path = load_report_path('ATP_reports_path')
             ATP_reports_path[country] = output_file
+            save_report_path('ATP_reports_path', ATP_reports_path)
+
             stock_sync_columns = ['cluster', 'category', 'shopid', 'shop_name', 'supplier_id', 'supplier_name',
                                   'shopee_merchANDiser', 'brand', 'sku_id', 'ItemName', 'ModelName', 'purchase_type',
                                   'stock_on_hand', 'organic_sales', 'be_stock']
@@ -251,7 +283,8 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
                                 'shopee_merchANDiser', 'brand', 'sku_id', 'ItemName', 'ModelName', 'purchase_type',
                                 'stock_on_hand', 'organic_sales', 'be_stock', 'pr_creation_date', 'last_pr_id']
             po_no_wh_stock_columns = ['cluster', 'category', 'shopid', 'shop_name', 'supplier_id', 'supplier_name',
-                                      'shopee_merchANDiser', 'brand', 'sku_id', 'ItemName', 'ModelName', 'purchase_type',
+                                      'shopee_merchANDiser', 'brand', 'sku_id', 'ItemName', 'ModelName',
+                                      'purchase_type',
                                       'stock_on_hand', 'organic_sales', 'be_stock', 'po_creation_date', 'last_order_id']
 
             stock_sync_ytd = ytd_write[ytd_write['stock_sync_issue'] == 1][stock_sync_columns]
@@ -286,42 +319,34 @@ def main(today_date, country_list, data_queried, files_processed, replenishment_
 
             print('It took', (time.time() - start) / 60, 'mins to complete the script.')
 
-    def find_file_path(pattern, root):
-        for x in os.listdir(root):
-            nd = os.path.join(root, x)
-            if pattern in x:
-                return nd
-
-
     if not summary_completed:
-        country_l = [ 'MY', 'TH', 'TW', 'ID', 'VN', 'PH']
-        if reports_written:
-            for i in country_l:
-                root='./'+i
-                ATP_reports_path[i]=find_file_path('Report',root) #can change to the report date
-                Replenishment_reports_path[i]=find_file_path('Tracker',root) #can change to the report date
+        country_l = ['MY', 'TH', 'TW', 'ID', 'VN', 'PH']
+        ATP_reports_path = load_report_path('ATP_reports_path')
+        Replenishment_reports_path = load_report_path('Replenishment_reports_path')
+
         print('Reading for templates!')
         Rep_summary_template = "./Summary templates/Regional_Replenishment_template_small.xlsx"
         ATP_summary_template = "./Summary templates/Regional_ATP_template_small.xlsx"
-        for c in range(len(ATP_reports_path)):
+        for c in range(len(country_l)):
             if not os.path.exists(ATP_reports_path[country_l[c]]):
-                print(ATP_reports_path[country_l[c]]+ ' not found')
+                print(ATP_reports_path[country_l[c]] + ' not found')
                 continue
             else:
-                print ('Reading ' + ATP_reports_path[country_l[c]])
+                print('Reading ' + ATP_reports_path[country_l[c]])
                 Regional_ATP_rewrite(ATP_reports_path[country_l[c]], ATP_summary_template, c)
                 print(ATP_reports_path[country_l[c]] + ' is done')
         print('Written for ATP Summary!')
 
-        for c in range(len(Replenishment_reports_path)):
+        for c in range(len(country_l)):
             if not os.path.exists(Replenishment_reports_path[country_l[c]]):
                 print(Replenishment_reports_path[country_l[c]] + ' not found')
                 continue
             else:
-                print ('Reading ' + Replenishment_reports_path[country_l[c]])
+                print('Reading ' + Replenishment_reports_path[country_l[c]])
                 Regional_Replenishment_rewrite(Replenishment_reports_path[country_l[c]], Rep_summary_template, c)
                 print(Replenishment_reports_path[country_l[c]] + ' is done')
         print('Written for Replenishment Summary!')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -330,10 +355,11 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--rep_done', help='has rep finished?', default=False)
     parser.add_argument('-c', '--countries', nargs='+', default=['ID', 'MY', 'TH', 'VN', 'TW', 'PH'])
     parser.add_argument("-d", "--date",
-                        default=dt.date.today() , #dt.date.today()
+                        default=dt.date.today(),  # dt.date.today()
                         type=lambda d: dt.datetime.strptime(d, '%Y%m%d').date(),
                         help="Date in the format yyyymmdd")
     parser.add_argument('-w', '--reports_written', help='has reports been written for ATP and Rep?', default=False)
     parser.add_argument('-s', '--summary_done', help='has summary been completed?', default=False)
     args = parser.parse_args()
-    main(args.date, args.countries, args.queried, args.processed, args.rep_done, args.reports_written, args.summary_done)
+    main(args.date, args.countries, args.queried, args.processed, args.rep_done, args.reports_written,
+         args.summary_done)
